b1636bc79167afce7fad11cc1e7844c87567e80a
Hitesh Sisara <hitesh.dev@sisara.in>
2024-03-28 10:08:19 +0530

    fibonacci

--- main.go
+++ main.go
@@ -1,10 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	fmt.Println(search("hitesh", "hite"))
+	n := 40
+
+	start := time.Now()
+	result := fibonaccir(n)
+	timetaken := time.Since(start)
+	fmt.Println("Recursive fibonacci(", n, ") result:", result, "Time:", timetaken)
+
+	start = time.Now()
+	result = fibonacci(n)
+	timetaken = time.Since(start)
+	fmt.Println(" fibonacci(", n, ") result:", result, "Time:", timetaken)
 }
 
-func search(haystack, needle string) bool {
+func fibonaccir(n int) int {
+	if n <= 1 {
+		return 1
+	}
+
+	return fibonaccir(n-1) + fibonaccir(n-2)
+}
+
+func fibonacci(n int) int {
+	if n <= 1 {
+		return 1
+	}
+
+	a, b := 0, 1
+
+	for i := 2; i <= n; i++ {
+
+		c := a + b
+		a = b
+		b = c
+
+	}
+
+	return b
 }
