8a72ae8fb74798964de1d560d05124c9cd895be9
Hitesh Sisara <hitesh.dev@sisara.in>
2024-03-28 09:25:21 +0530

    string search

--- main.go
+++ main.go
@@ -7,4 +7,24 @@ func main() {
 }
 
 func search(haystack, needle string) bool {
+	if len(needle) == 0 {
+		return true
+	}
+	for i := 0; i < len(haystack)-len(needle); i++ {
+
+		match := true
+
+		for j := 0; j < len(needle); j++ {
+			if haystack[i+j] != needle[j] {
+				match = false
+				break
+			}
+		}
+
+		if match {
+			return true
+		}
+	}
+
+	return false
 }
