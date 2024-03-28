c1ddf1dadb9c0c44492f65490b2536c7e7a0840f
Hitesh Sisara <hitesh.dev@sisara.in>
2024-03-28 13:38:30 +0530

    odd even using go routine

--- main.go
+++ main.go
@@ -3,8 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(search("hitesh", "hite"))
+	oddch := make(chan int)
+	evench := make(chan int)
+
+	go PrintNum(oddch, 1)
+	go PrintNum(evench, 2)
+
+	for i := 0; i < 100; i++ {
+		select {
+		case num := <-oddch:
+			fmt.Println(num)
+		case num := <-evench:
+			fmt.Println(num)
+
+		}
+	}
 }
 
-func search(haystack, needle string) bool {
+func PrintNum(ch chan int, start int) {
+	for i := start; i <= 100; i++ {
+		ch <- i
+	}
 }
